aae8f3d5b5a2b2fd3ff6e04ab17caf73c3c63f79
Long Do Cao <long@homingbird.ai>
2021-08-03 15:24:00 +0200

    feat: execute create table

--- restart/install_postgresql.sh
+++ restart/install_postgresql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+export PGPASSWORD=K2f7mstuzakFFspH
 sudo apt-get update
 sudo apt-get install -y htop iotop emacs
 sudo apt-get install -y postgresql postgresql-contrib
@@ -8,5 +8,7 @@ sudo apt-get install -y postgresql postgresql-contrib
 # sudo su postgres
 # psql
 # ALTER USER postgres WITH PASSWORD 'K2f7mstuzakFFspH';
-# modify /etc/postgresql/13/main/pg_hba.conf change peer > md5 (see https://stackoverflow.com/questions/18664074/getting-error-peer-authentication-failed-for-user-postgres-when-trying-to-ge)
+# modify /etc/postgresql/9.5/main/pg_hba.conf change peer > md5 (see https://stackoverflow.com/questions/18664074/getting-error-peer-authentication-failed-for-user-postgres-when-trying-to-ge)
 # sudo service postgresql restart
+psql -U postgres -f denormalized.sql
+psql -U postgres -f normalized.sql

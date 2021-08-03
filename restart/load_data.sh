9123764841ddddd771782d9ceee6d374823c2e1d
Long Do Cao <long@homingbird.ai>
2021-08-03 18:17:24 +0200

    fix: weather

--- restart/load_data.sh
+++ restart/load_data.sh
@@ -4,6 +4,7 @@ export PGPASSWORD=K2f7mstuzakFFspH
 
 psql -U postgres -f denormalized.sql
 psql -U postgres -f normalized.sql
+psql -d postgres -U postgres -p 5432 -c "\copy normalized_weather FROM PROGRAM 'gzip -dc $HOME/nyc-taxi-data/normalized/weather_xaa.csv.gz' DELIMITER ',' CSV HEADER NULL ''" &
 
 
 files=`ls -1 $HOME/nyc-taxi-data/denormalized/*.csv.gz`
@@ -15,7 +16,7 @@ done
 
 wait
 
-files=`ls -1 $HOME/nyc-taxi-data/normalized/*.csv.gz`
+files=`ls -1 $HOME/nyc-taxi-data/normalized/trips*.csv.gz`
 for f in $files; do
     ((i=i%N)); ((i++==0)) && wait
     psql -d postgres -U postgres -p 5432 -c "\copy normalized_trips FROM PROGRAM 'gzip -dc $f' DELIMITER ',' CSV HEADER NULL ''" &

b951b7f850f8a54f258c5cfeba3e61a17a9fbebc
Long Do Cao <long@homingbird.ai>
2021-08-03 15:58:15 +0200

    perf: parallel only 4 at a time

--- restart/load_data.sh
+++ restart/load_data.sh
@@ -7,9 +7,13 @@ psql -U postgres -f normalized.sql
 
 
 files=`ls -1 $HOME/nyc-taxi-data/denormalized/*.csv.gz`
+N=`grep -c ^processor /proc/cpuinfo`
 for f in $files; do
+    ((i=i%N)); ((i++==0)) && wait #parallel run only N at a time ; see https://unix.stackexchange.com/questions/103920/parallelize-a-bash-for-loop
     psql -d postgres -U postgres -p 5432 -c "\copy denormalized_trips FROM PROGRAM 'gzip -dc $f' DELIMITER ',' CSV HEADER NULL ''" &
 done
+
+wait
 
 files=`ls -1 $HOME/nyc-taxi-data/normalized/*.csv.gz`
 for f in $files; do

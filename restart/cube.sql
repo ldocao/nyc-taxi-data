fefd5984e4f2cc4f6e389e85d7ac191494c83f97
Long Do Cao <long@homingbird.ai>
2021-09-23 15:05:31 +0200

    fix: remove vendor_id

--- restart/cube.sql
+++ restart/cube.sql
@@ -1,9 +1,9 @@
 drop table if exists cube_trips;
 
 create table cube_trips as (
-    select nt.vendor_id, extract(year from nw.date) as year, nw.max_temperature, count(trip_id) as count
+    select extract(year from nw.date) as year, nw.max_temperature, count(trip_id) as count
     from normalized_trips nt
     left join normalized_weather nw 
     on nt.pickup_datetime::date = nw.date
-    group by cube(nt.vendor_id, extract(year from nw.date), nw.max_temperature)
+    group by cube(extract(year from nw.date), nw.max_temperature)
 );
--- restart/index.sql
+++ restart/index.sql
@@ -1,12 +1,12 @@
 create index normalized_trips_hash_trip on normalized_trips using hash (trip_id);
 
-UPDATE pg_index
-SET indisvalid=false
-WHERE indrelid = (
-    SELECT oid
-    FROM pg_class
-    WHERE relname='normalized_trips'
-); -- we disable index because we first start with a query without index
+-- UPDATE pg_index
+-- SET indisvalid=false
+-- WHERE indrelid = (
+--     SELECT oid
+--     FROM pg_class
+--     WHERE relname='normalized_trips'
+-- ); -- we disable index because we first start with a query without index
 
 
 -- to re-enable index, the instructor should at some point run the query below
